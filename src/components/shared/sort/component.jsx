4069f089bef5b4e074a5570b08ca5b42dc9a267e
Ольга Захарова <kotfimn@mail.ru>
2026-01-25 21:07:02 +0700

    сортировка по дате

--- src/components/shared/sort/component.jsx
+++ src/components/shared/sort/component.jsx
@@ -5,9 +5,11 @@ export const Sort = ({isSorted, sortList}) => {
   const className = cn('sort', {
     'sorted' : isSorted
   })
+
+  const comment = '// сортировка по дате'
   return (
     <div className={className} onClick={sortList}>
-      <span>quick-sort.js</span> <span>// сортировка по дате</span>
+      <span>quick-sort.js</span> <span>{comment}</span>
     </div>
   )
 }
